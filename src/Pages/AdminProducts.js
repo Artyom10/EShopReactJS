a232108370f9d5eb3cb11bd4b453836f31387446
Artyom <43495856+Artyom10@users.noreply.github.com>
2020-11-23 19:35:27 +0300

    changes

--- src/Pages/AdminProducts.js
+++ src/Pages/AdminProducts.js
@@ -1,7 +1,7 @@
 import logo from '../logo.svg';
 import React from 'react';
 import Nav from '../Components/MainPage/Navbar/Nav';
-import AddProductButton from '../Components/MainPage/AddPoductButton/AddProductButton';
+import AddProductButton from '../Components/MainPage/ProductsList/AddPoductButton/AddProductButton';
 import ProductsList from '../Components/MainPage/ProductsList/ProductsList';
 import Footer from '../Components/MainPage/Footer/Footer';
 import '../StylesForPages/AdminProducts.css';
