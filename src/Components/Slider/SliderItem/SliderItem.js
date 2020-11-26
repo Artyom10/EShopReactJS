2edb034f7f098ddfbdc7948d17c7777233904d66
Artyom <43495856+Artyom10@users.noreply.github.com>
2020-11-26 13:55:07 +0300

    fix slider

--- src/Components/Slider/SliderItem/SliderItem.js
+++ src/Components/Slider/SliderItem/SliderItem.js
@@ -5,8 +5,15 @@ function SliderItem(props) {
     const {info} = props;
     const { urlPhoto, text, id} = info;
     console.log(props,info);
+    let classActive = null;
+    if(id === 1){
+       classActive = 'carousel-item active'
+    }
+    else{
+      classActive = 'carousel-item'
+    }
     return (
-      <div className="carousel-item">
+      <div className={classActive}>
         <img src={urlPhoto} className="d-block w-100" alt="photo" />
         <div className="carousel-caption d-none d-md-block">
        <p className="carusel-description">{text}</p>
