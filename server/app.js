4d06033cbc47e6da10a58bbc44030656596a08ca
Artyom10 <artyomsunbright@gmail.com>
2020-12-15 15:15:17 +0300

    changes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const admin = require('firebase-admin');
+//const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
+let adminRouter = require("./routes/adminRouter");
 
 const PORT = 5000;
 
 const app = express();
 
-//app.engine()
+
+app.use("/", adminRouter);
+
+app.get('/', (req, res) => {
+    res.send('"What do you need on this server?')
+})
 
 
 
