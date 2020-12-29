62b5c65286f4d383f62ee3c68a21a3b4b7d816f9
Artyom <43495856+Artyom10@users.noreply.github.com>
2020-12-30 02:02:56 +0300

    minor changes

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: process.env.SERVER_BASE_URL,
+  // baseURL: process.env.SERVER_BASE_URL,
+  baseURL: "https://simple-eshop-project.herokuapp.com/",
 });
